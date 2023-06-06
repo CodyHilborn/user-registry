2e586092aba782a90151f349232096eda0a1d2d0
Cody Hilborn <codyhilborn11@gmail.com>
2023-06-06 13:05:28 -0400

    style components using css modules

--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -1,19 +1,22 @@
 import React from "react";
+import Card from "../UI/Card";
+import styles from "./AddUser.module.css";
 
 const AddUser = (props) => {
-
   const addUserHandler = (event) => {
     event.preventDefault();
-  }
+  };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Age (in years)</label>
-      <input id="age" type="text" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (in years)</label>
+        <input id="age" type="text" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
