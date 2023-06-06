e43459d766d54ce30066aa617b477e21d613cba4
Cody Hilborn <codyhilborn11@gmail.com>
2023-06-06 13:40:22 -0400

    implement a user list with dynamically updated ui

--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -1,20 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
+import Button from "../UI/Button";
 import styles from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  const [inputUsername, setInputUsername] = useState("");
+  const [inputAge, setInputAge] = useState("");
+
+  const usernameChangeHandler = (event) => {
+    setInputUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setInputAge(event.target.value);
+  };
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (inputUsername.trim().length === 0 || inputAge.trim().length === 0) {
+      return;
+    }
+
+    if (+inputAge < 0) {
+      return;
+    }
+
+    props.usersHandler({ username: inputUsername, age: inputAge });
+
+    setInputUsername("");
+    setInputAge("");
   };
 
   return (
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input
+          id="username"
+          type="text"
+          value={inputUsername}
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor="age">Age (in years)</label>
-        <input id="age" type="text" />
-        <button type="submit">Add User</button>
+        <input
+          id="age"
+          type="text"
+          value={inputAge}
+          onChange={ageChangeHandler}
+        />
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
